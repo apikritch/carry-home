c3a438550ed11a70ac4a7561d9abdb9c4351ddd9
Apikritch Rattanapisankul <apikritch.r@gmail.com>
2023-08-08 04:13:13 +1000

    chaged api

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default () => {
   return axios.create({
-    baseURL: "http://www.apikritch.com:8003/",
+    baseURL: "http://portfolio.apikritch.com:8003/",
   });
 };
